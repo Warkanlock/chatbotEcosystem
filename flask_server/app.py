9156ea6347e42035feea73f278f8c3243bafb92d
ignacio-b-brasca <ignacio.b.brasca@us.pwc.com>
2019-07-03 10:09:12 -0300

    GUI interface

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,14 +1,19 @@
 from chatbot import query
 from chatbot import answers
-from flask import Flask
+from flask import Flask, render_template, request
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./templates')
 
 test_temp = query.QueryManager()
 
 
-@app.route('/index')
+@app.route('/')
+@app.route('/index', methods=['GET', 'POST'])
 def index():
-    response = test_temp.query('How many sections are?')
-    hello = test_temp.manageResponse(response)
-    return hello
+    if request.method == 'GET':
+        return render_template('index.html', text='Just ask me anything about US CODE...')
+    if request.method == 'POST':
+        question = request.form
+        response = test_temp.query(question['question'])
+        text = test_temp.manageResponse(response)
+        return text
