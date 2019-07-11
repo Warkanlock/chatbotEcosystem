249a7a45bfad42a2e4a5f94ab0c0352ac1e634fc
ignacio-b-brasca <ignacio.b.brasca@us.pwc.com>
2019-07-11 09:53:42 -0300

    Webhook implement

--- flask_server/app.py
+++ flask_server/app.py
@@ -1,10 +1,12 @@
 from chatbot import query
 from chatbot import answers
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
+import requests
 
 app = Flask(__name__, template_folder='./templates')
 
 test_temp = query.QueryManager()
+HOST = 'http://127.0.0.1:5000'
 
 
 @app.route('/')
@@ -17,3 +19,30 @@ def index():
         response = test_temp.query(question['question'])
         text = test_temp.manageResponse(response)
         return text
+
+
+def findnumbersection(text_response):
+    try:
+        response = requests.get(HOST + '/graph_api/CountNodes')
+        response = response.json()
+        print(response)
+        replace = text_response.replace(r"@number", str(response[0][0]))
+    except ReferenceError as e:
+        return -1
+    return replace
+
+
+@app.route('/chatbot', methods=['POST'])
+def chatbot():
+    data = request.get_json(silent=True)
+    if data['queryResult']['action'] == 'input.welcome':
+        reply = {
+            "fulfillmentText": "Hi, I'm Chatboy.  [ PwC Proof of Concept ]",
+        }
+    elif data['queryResult']['action'] == 'find.number.section':
+        responseText = findnumbersection(
+            data['queryResult']['fulfillmentMessages'][0]['text']['text'][0])
+        reply = {
+            "fulfillmentText": responseText
+        }
+    return jsonify(reply)
